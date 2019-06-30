9d50150f3e6a0542a778c7ae5528f5a21ae3fd0b
Walmyr Filho <limaesilva@qelp.com>
2019-06-30 14:23:46 +0200

    Move test function to support commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("createVideo", video => {
+  cy.visit("videos/create");
+
+  cy.get("#video-title-input")
+    .type(video.title);
+  cy.get("#video-description-input")
+    .type(video.description);
+  cy.get("#video-url-input")
+    .type(video.url);
+
+  cy.get("#submit-button").click();
+});
