a00f29a23efbc02ac8a89fca38597b5b8addf641
Walmyr Filho <limaesilva@qelp.com>
2019-08-10 03:35:20 +0200

    Scope browser ui tests and use new cy command

--- cypress/integration/vidbits.spec.js
+++ cypress/integration/vidbits.spec.js
@@ -3,13 +3,27 @@ describe("Vidbits", () => {
 
   beforeEach(() => cy.exec("npm run drop-db"));
 
-  context("Empty state", () => {
-    it("creates two videos", () => {
-      videos.forEach(video => cy.createVideo(video));
+  context("Browser UI", () => {
+    context("Empty state", () => {
+      it("creates two videos", () => {
+        videos.forEach(video => cy.createVideo(video));
 
-      cy.visit("videos");
+        cy.visit("videos");
 
-      cy.get(".video-card").its("length").should("eq", 2);
+        cy.validateNumberOfVideoCardsEqualTo(2);
+      });
+    });
+
+    context("Two items already seeded in the DB", () => {
+      beforeEach(() => cy.exec("npm run seed-db"));
+
+      it("creates two more videos", () => {
+        videos.forEach(video => cy.createVideo(video));
+
+        cy.visit("videos");
+
+        cy.validateNumberOfVideoCardsEqualTo(4);
+      });
     });
   });
 
