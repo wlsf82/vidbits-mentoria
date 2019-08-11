0611f963fe2adc2d5ca790d16f05e39fdfa1095c
Walmyr Filho <limaesilva@qelp.com>
2019-08-11 22:41:47 +0200

    Delete lots of tests and update dependencies

--- test/e2e/page-objects/video.js
+++
@@ -1,9 +0,0 @@
-class Video {
-  constructor() {
-    this.container = element(by.className("video-card"));
-    this.deleteButton = this.container.element(by.css(".video-buttons #delete"));
-    this.editButton = this.container.element(by.css(".video-buttons #edit"));
-  }
-}
-
-module.exports = Video;
--- test/e2e/protractorFirefox.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports.config = require("./createProtractorConfig.js")({
-    capabilities: {
-        browserName: "firefox",
-        "marionette": true,
-        "moz:firefoxOptions": {
-            "args": ["--headless"]
-        }
-    }
-});
